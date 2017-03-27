f434a5bd7ba66508973703dcf053d11d34496c71
Jyotsna-Singh <jyotsna_singh94@yahoo.co.in>
2017-03-27 23:15:54 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -88,7 +88,7 @@ function nextPage(){
 			q: q,
 			pageToken: token,
 			type:'video',
-			key: 'AIzaSyCvk3NNMQASZgFkCNxIp9jH-l8O0PXhDUo'},
+			key: 'YOUR_API_KEY_HERE'},
 			function(data){
 				var nextPageToken = data.nextPageToken;
 				var prevPageToken = data.prevPageToken;
@@ -132,7 +132,7 @@ function prevPage(){
 			q: q,
 			pageToken: token,
 			type:'video',
-			key: 'AIzaSyCvk3NNMQASZgFkCNxIp9jH-l8O0PXhDUo'},
+			key: 'YOUR_API_KEY_HERE'},
 			function(data){
 				var nextPageToken = data.nextPageToken;
 				var prevPageToken = data.prevPageToken;
